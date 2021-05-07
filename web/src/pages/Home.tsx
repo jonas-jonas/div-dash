221d8cd76365b5d4d1071ec3032418dae7ec01dd
Jonas Hungershausen <jonas.hungershausen@adesso.de>
2021-05-07 17:38:17 +0200

    Remove react app

--- web/src/pages/Home.tsx
+++
@@ -1,7 +0,0 @@
-import { useRecoilValue } from "recoil";
-import { userState } from "../state/authState";
-
-export function Home() {
-  const user = useRecoilValue(userState);
-  return <div className="container">Hi {user?.email || "Not logged in"}</div>;
-}
