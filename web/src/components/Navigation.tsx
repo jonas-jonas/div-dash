3c11757dcdc796e3dfb217f220b8bbf70ea43cd9
Jonas Hungershausen <jonas.hungershausen@gmail.com>
2021-07-30 10:14:30 +0200

    Adjust navigation style to design

--- web/src/components/Navigation.tsx
+++ web/src/components/Navigation.tsx
@@ -1,3 +1,5 @@
+import { faFileInvoice, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classNames from "classnames";
 import ky from "ky";
 import { Link, useLocation } from "react-router-dom";
@@ -15,10 +17,10 @@ export function Navigation() {
 
   const navItemClasses = (pathName: string) => {
     return classNames(
-      "ml-6 h-full flex items-center border-b-4 border-transparent pt-1 px-3 transition-colors",
+      "ml-20 h-full flex items-center border-b-4 border-transparent pt-1 px-3 transition-colors font-bold",
       {
-        "border-gray-50": location.pathname === pathName,
-        "hover:border-gray-400": location.pathname !== pathName,
+        "border-gray-50 text-white": location.pathname === pathName,
+        "hover:border-gray-400 text-gray-500": location.pathname !== pathName,
       }
     );
   };
@@ -32,40 +34,19 @@ export function Navigation() {
       <div className="container mx-auto flex justify-between items-stretch">
         <div className="flex items-center">
           <Link className="navbar-item" to="/">
-            <img src="/assets/logo.svg" alt="Logo" width="112" height="28" />
+            <img src="/logo-white@2x.png" alt="Logo" width="112" height="28" />
           </Link>
           <Link className={navItemClasses("/accounts")} to="/accounts">
-            Accounts
+            <FontAwesomeIcon icon={faFileInvoice} className="mr-3" />
+            <span className="tracking-wider">Accounts</span>
           </Link>
         </div>
-
         <div className="flex items-center">
-          {user && (
-            <>
-              <p className="mr-2">{user.email}</p>
-              <div className="buttons">
-                <button
-                  className="px-4 py-2 bg-white shadow rounded text-gray-900"
-                  onClick={handleLogout}
-                >
-                  <strong>Logout</strong>
-                </button>
-              </div>
-            </>
-          )}
-          {!user && (
-            <>
-              <Link className="mr-6" to="/login">
-                Log in
-              </Link>
-              <Link
-                className="px-4 py-2 bg-white shadow rounded text-gray-900"
-                to="/register"
-              >
-                <strong>Sign up</strong>
-              </Link>
-            </>
-          )}
+          <button className="px-3 py-2">
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
+          <span className="mx-5 border-l-2 border-white h-4/6 block"></span>
+          <button onClick={handleLogout} className="px-3 py-2">{user?.email}</button>
         </div>
       </div>
     </nav>
--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -5,7 +5,7 @@ import { PortfolioGraph } from "../components/PortfolioGraph";
 export function Home() {
   return (
     <div className="container mb-24 mx-auto">
-      <div className="grid grid-cols-3 gap-4 p-8">
+      <div className="grid grid-cols-3 gap-4 py-8">
         <PortfolioGraph />
 
         <PortfolioComposition />
