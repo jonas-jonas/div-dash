5da3f7e8d193f2f4a67ed9c572be9b4f7f1baac9
Jonas Hungershausen <jonas.hungershausen@gmail.com>
2021-09-04 20:27:41 +0200

    Add profile navigation flyout

--- web/src/components/Navigation.tsx
+++ web/src/components/Navigation.tsx
@@ -1,8 +1,16 @@
 import { faBitcoin } from "@fortawesome/free-brands-svg-icons";
-import { faFileInvoice, faFileInvoiceDollar, faSearch } from "@fortawesome/free-solid-svg-icons";
+import {
+  faChevronDown,
+  faFileInvoice,
+  faFileInvoiceDollar,
+  faSearch,
+  faSignOutAlt,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classNames from "classnames";
 import ky from "ky";
+import { useCallback, useEffect, useState } from "react";
 import { useMutation, useQuery, useQueryClient } from "react-query";
 import { Link, useLocation } from "react-router-dom";
 import { User } from "../models/user";
@@ -13,11 +21,17 @@ export function Navigation() {
   const location = useLocation();
   const queryClient = useQueryClient();
 
+  const [showUserMenu, setShowUserMenu] = useState<boolean>(false);
+
   const logoutMutation = useMutation<void, ky.HTTPError>(api.getLogout, {
     onSettled: () => {
       queryClient.setQueryData("identity", null);
     },
   });
+
+  const handleUserClick = useCallback(() => {
+    setShowUserMenu((show) => !show);
+  }, []);
 
   const handleLogout = async () => {
     logoutMutation.mutate();
@@ -33,6 +47,18 @@ export function Navigation() {
       }
     );
   };
+
+  useEffect(() => {
+    const escListener = function (event: KeyboardEvent) {
+      if (event.key === "Escape") {
+        setShowUserMenu(false);
+      }
+    };
+    document.addEventListener("keydown", escListener);
+    return () => {
+      document.removeEventListener("keydown", escListener);
+    };
+  }, []);
 
   return (
     <nav
@@ -53,7 +79,10 @@ export function Navigation() {
             <FontAwesomeIcon icon={faFileInvoiceDollar} className="mr-3" />
             <span className="tracking-wider">Stocks</span>
           </Link>
-          <Link className={navItemClasses("/symbols/crypto")} to="/symbols/crypto">
+          <Link
+            className={navItemClasses("/symbols/crypto")}
+            to="/symbols/crypto"
+          >
             <FontAwesomeIcon icon={faBitcoin} className="mr-3" />
             <span className="tracking-wider">Cryptocurrencies</span>
           </Link>
@@ -63,9 +92,28 @@ export function Navigation() {
             <FontAwesomeIcon icon={faSearch} />
           </button>
           <span className="mx-5 border-l-2 border-white h-4/6 block"></span>
-          <button onClick={handleLogout} className="px-3 py-2">
-            {user?.email}
-          </button>
+          <div className="relative">
+            <button onClick={handleUserClick} className="px-3 py-2 focus:underline focus:outline-none">
+              <span className="mr-2">{user?.email}</span>
+              <FontAwesomeIcon icon={faChevronDown} size="xs" />
+            </button>
+            {showUserMenu && (
+              <div className="absolute right-0 bg-white rounded shadow-xl border p-2 text-gray-800 flex flex-col w-full mt-4">
+                <div className="absolute -top-2 w-4 h-4 right-0 -translate-x-1/2 transform rotate-45 bg-white border-l border-t"></div>
+                <button className="px-4 text-left rounded hover:bg-gray-100 transition-colors relative flex items-center py-2">
+                  <FontAwesomeIcon icon={faUser} className="absolute" />
+                  <span className="ml-8 block font-bold">Profile</span>
+                </button>
+                <button
+                  className="px-4 text-left rounded hover:bg-gray-100 transition-colors relative flex items-center py-2"
+                  onClick={handleLogout}
+                >
+                  <FontAwesomeIcon icon={faSignOutAlt} className="absolute" />
+                  <span className="ml-8 block font-bold">Logout</span>
+                </button>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </nav>
